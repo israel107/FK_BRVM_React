eee73b09bd474bd038618324ad52267585038c2d
israel107 <koupohchristian@gmail.com>
2025-12-21 18:54:34 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button>Change !</Button>
+    
+ 
+    </div>
+  );
+}
